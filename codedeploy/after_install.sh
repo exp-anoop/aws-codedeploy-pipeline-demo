e7458f4c9c816b975dc8f15c94cf1a0ee7423452
Anoop P R <anoop.pr@experionglobal.com>
2018-05-16 14:14:21 +0530

    Deployment configuration changed

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -5,4 +5,4 @@ cd /home/ubuntu/app
 sudo chmod -R 777 /home/ubuntu/app/
 /home/ubuntu/.nvm/versions/node/v8.11.2/bin/npm install
 
-/home/ubuntu/.nvm/versions/node/v8.11.2/bin/pm2 stop all
+# /home/ubuntu/.nvm/versions/node/v8.11.2/bin/pm2 stop all
