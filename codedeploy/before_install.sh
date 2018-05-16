63cce85de7636cf161194d78bf5152921979dd87
Anoop P R <anoop.pr@experionglobal.com>
2018-05-16 13:17:16 +0530

    Deployment configuration changed

--- codedeploy/before_install.sh
+++ codedeploy/before_install.sh
@@ -8,8 +8,8 @@ export app_root=/home/ubuntu/app
 if [ -d "$app_root" ];then
     rm -rf /home/ubuntu/app
     mkdir -p /home/ubuntu/app
-    chmod -R 777 /home/ubuntu/app
+    sudo chmod -R 777 /home/ubuntu/app
 else
     mkdir -p /home/ubuntu/app
-    chmod -R 777 /home/ubuntu/app
+    sudo chmod -R 777 /home/ubuntu/app
 fi
