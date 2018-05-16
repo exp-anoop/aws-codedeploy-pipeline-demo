bc2387d52a97ded227884357ad7a3ec940ed33a2
Anoop P R <anoop.pr@experionglobal.com>
2018-05-16 13:22:59 +0530

    Deployment configuration changed

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -2,6 +2,7 @@
 
 # This script is executed after the source is copied to the instances
 cd /home/ubuntu/app
+sudo chmod -R 777 codedeploy/
 /home/ubuntu/.nvm/versions/node/v8.11.2/bin/npm install
 
 /home/ubuntu/.nvm/versions/node/v8.11.2/bin/pm2 stop all
